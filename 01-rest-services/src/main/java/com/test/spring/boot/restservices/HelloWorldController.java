8149590532da3056dda15a1f5b656a2484527555
jobsformilind <jobs.for.milind@gmail.com>
2018-02-02 22:00:55 -0500

    Commiting changes

---
+++ 01-rest-services/src/main/java/com/test/spring/boot/restservices/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.test.spring.boot.restservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@GetMapping(path = "/hello")
+	public String hello() {
+		return "Hello Spring";
+	}
+}
