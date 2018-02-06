e355db9c23d28a6ae9bac02a762c89d0b25de6f5
jobsformilind <jobs.for.milind@gmail.com>
2018-02-05 20:45:20 -0500

    Commiting changes

--- 01-rest-services/src/main/java/com/test/spring/boot/restservices/RestServicesApplication.java
+++ 01-rest-services/src/main/java/com/test/spring/boot/restservices/RestServicesApplication.java
@@ -1,12 +1,32 @@
 package com.test.spring.boot.restservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServicesApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		return source;
 	}
 }
--- 01-rest-services/src/main/java/com/test/spring/boot/restservices/hello/HelloWorldController.java
+++ 01-rest-services/src/main/java/com/test/spring/boot/restservices/hello/HelloWorldController.java
@@ -1,22 +1,34 @@
 package com.test.spring.boot.restservices.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
+
+	@Autowired
+	MessageSource messageSource;
+
 	@GetMapping(path = "/hello")
 	public String hello() {
 		return "Hello Spring";
+	}
+
+	@GetMapping(path = "/hello/i18n")
+	public String helloi18n(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 
 	@GetMapping(path = "/hello/bean")
 	public HelloBean helloBean() {
 		return new HelloBean("Hello Spring Bean");
 	}
-
 
 	@GetMapping(path = "/hello/{name}")
 	public HelloBean helloBeanPath(@PathVariable String name) {
